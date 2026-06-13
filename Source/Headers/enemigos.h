c70de0de7c97d7c54f5dffbc1c194304c277cfd4
Matías Bulacio <adrianmatiasbulacio@gmail.com>
2026-06-13 17:04:23 -0300

    Dibujar pistola

--- Source/Headers/enemigos.h
+++ Source/Headers/enemigos.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <raylib.h>
+#include <stddef.h>
 typedef struct {
 	Vector2 coordenadas;
+	Texture2D *textura;
 } Enemigo;
 
 void Enemigo_Avanzar(Enemigo *e, int velocidad, float frame_time);
+
+void Enemigo_DibujarVarios(Enemigo* e, size_t cantidad);
--- Source/Implementation/shooter.c
+++ Source/Implementation/shooter.c
@@ -12,50 +12,66 @@ Image image;
 Texture2D texture;
 size_t cantidad_enemigos = 5;
 
+int posicion_pistola = 416;
+
 int setup_shooter() {
-        if (arreglo_de_enemigos == NULL)
-            free(arreglo_de_enemigos);
-        cantidad_enemigos = 5;
+    if (IsTextureValid(texture)) {
+        UnloadTexture(texture);
+    }
+    if (IsImageValid(image)) {
+        UnloadImage(image);
+    }
 
-        arreglo_de_enemigos =
-            calloc(cantidad_enemigos, sizeof(*arreglo_de_enemigos));
+    image = LoadImage("Resources/Animals/tiger.png");
+    ImageResizeNN(&image, 128, 128);
+    texture = LoadTextureFromImage(image);
 
-        if (arreglo_de_enemigos == NULL) {
-            return -1; // Error
-        }
+    if (arreglo_de_enemigos == NULL)
+        free(arreglo_de_enemigos);
+    cantidad_enemigos = 5;
 
-        for (size_t i = 0; i < cantidad_enemigos; i++) {
-            Enemigo *e = &arreglo_de_enemigos[i];
-            e->coordenadas.y = 0;
-            e->coordenadas.x = 64 + 176 * i;
-        }
-        if (IsTextureValid(texture)) {
-            UnloadTexture(texture);
-        }
-        image = LoadImage("Resources/Animals/tiger.png");
-		ImageResizeNN(&image, 128, 128);
-		texture = LoadTextureFromImage(image);
-		return 0;
+    arreglo_de_enemigos =
+        calloc(cantidad_enemigos, sizeof(*arreglo_de_enemigos));
+
+    if (arreglo_de_enemigos == NULL) {
+        return -1; // Error
+    }
+
+    for (size_t i = 0; i < cantidad_enemigos; i++) {
+        Enemigo *e = &arreglo_de_enemigos[i];
+        e->coordenadas.y = 0;
+        e->coordenadas.x = 64 + 176 * i;
+        e->textura = &texture;
+    }
+    return 0;
 }
 
 int shooter(bool setup) {
-    if (setup) { // Seting up
-		int r = setup_shooter();
-		if (r != 0) return r;
+	float delta = GetFrameTime();
+    if (setup) { // Setting up
+        int r = setup_shooter();
+        if (r != 0)
+            return r;
     }
+    for (size_t i = 0; i < cantidad_enemigos; i++) {
+        Enemigo_Avanzar(&arreglo_de_enemigos[i], 75, delta);
+    }
+
+    int movimiento_pistola = delta * 400 * ((IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) -
+                                    (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)));
+	 posicion_pistola += movimiento_pistola;
+
     ClearBackground(BEIGE);
 
-	DrawRectangle(0, 500, 832, 20, ColorAlpha(WHITE, 0.8));
-    for (size_t i = 0; i < cantidad_enemigos; i++) {
-        Enemigo *e = &arreglo_de_enemigos[i];
-        Enemigo_Avanzar(e, 75, GetFrameTime());
-        DrawTexture(texture, e->coordenadas.x - 64, e->coordenadas.y - 64, WHITE);
-    }
+    DrawRectangle(0, 500, 832, 20, ColorAlpha(WHITE, 0.8));
 
-	char buf[200];
-	sprintf(buf, "Puntos: %u", puntos);
+    Enemigo_DibujarVarios(arreglo_de_enemigos, cantidad_enemigos);
 
-	DrawText(buf, 20, 20, 24, BLACK);
+    char buf[200];
+    sprintf(buf, "Puntos: %u", puntos);
 
+    DrawText(buf, 20, 20, 24, BLACK);
+
+    DrawRectangle(posicion_pistola - 16, 550, 32, 90, DARKGRAY);
     return 1;
 }
